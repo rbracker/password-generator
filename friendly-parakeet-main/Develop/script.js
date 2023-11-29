a54a7eca09b6d691193257ac730cbeda2ab31073
Ranee Bracker <rbracker@asu.edu>
2023-11-29 00:18:54 -0700

    final update on code

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -8,9 +8,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var passwordLength = 8;
+  var generatedPassword = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    var randomChar = charset.charAt(randomIndex);
+
+    generatedPassword += randomChar;
+  }
+
+  return generatedPassword;
 }
 
 // Add event listener to generate button
